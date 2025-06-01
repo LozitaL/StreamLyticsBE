d9cc33d7f9b9bf6c9af0cb023e432c97be712be3
lozita14 <lozanoruizj@gmail.com>
2025-06-01 15:51:31 +0200

    a

--- db/conMongoDB.js
+++ db/conMongoDB.js
@@ -6,12 +6,17 @@ const uri = process.env.MONGO_URI;
 let client;
 
 export const getConnection = async () => {
+ try {
   if (!client) {
     client = new MongoClient(uri);
     await client.connect();
     console.log('Connected to MongoDB');
   }
   return client;
+} catch (error) {
+  console.error('Error connecting to MongoDB:', error);
+  throw new Error('Failed to connect to MongoDB');
+}
 };
 
 export const getDb = async (dbName = 'streamlytics') => {
